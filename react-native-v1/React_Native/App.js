dae045348bdf8fc338bda9cf95341aeff5dea1dd
Rawabi Okour <rawabi.f.okour@gmail.com>
2019-09-22 22:50:57 +0300

    update

--- react-native-v1/React_Native/App.js
+++ react-native-v1/React_Native/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Numbers from './Screens/Numbers'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      <Text>Welcome</Text>
+      {/* <Numbers/> */}
     </View>
   );
 }
