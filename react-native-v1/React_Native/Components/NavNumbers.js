30ec7a66ace6cf15eaefe4010dbcfe4257232929
Rawabi Okour <rawabi.f.okour@gmail.com>
2019-09-22 17:56:31 +0300

    update

---
+++ react-native-v1/React_Native/Components/NavNumbers.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { View,Text } from 'react-native';
+export default class NavNumbers extends Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    Rawabi
+                </Text>
+            </View>
+        )
+    }
+}
---
+++ react-native-v1/React_Native/Screens/Numbers.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import NavNumbers from '../Components/NavNumbers';
+import SliderNumbers from '../Components/SliderNumbers';
+import { View } from 'react-native';
+export default class Numbers extends Component {
+    render() {
+        return (
+            <View>
+            <NavNumbers/>
+            <SliderNumbers/>
+            </View>
+        )
+    }
+}
