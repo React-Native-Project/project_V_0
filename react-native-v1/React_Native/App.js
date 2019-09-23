ebc18ecca3eb31840b6d28e8cb84965c52bc1355
Rawabi Okour <rawabi.f.okour@gmail.com>
2019-09-23 09:51:12 +0300

    new

--- react-native-v1/React_Native/App.js
+++ react-native-v1/React_Native/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       {/* <Text>Open up App.js to start working on your app!</Text> */}
-      <Text>Welcome</Text>
+      <Text>Welcome rawabi</Text>
       {/* <Numbers/> */}
     </View>
   );
